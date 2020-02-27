954b9613d24bfdc42e17187f03d54ae628f85e37
Jacob Whisler <jwhisler521@gmail.com>
2020-02-26 21:02:58 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql')
 
-var connection;
+const connection;
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: 'hh9jcrbfadbugcmg:ntm4yep39ot8els9@z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'hh9jcrbfadbugcmg',
-  password: 'ntm4yep39ot8els9',
-  database: 'ioisqzm06v1oymye'
+  host: 'localhost',
+  user: 'root',
+  password: 'root521',
+  database: 'burgersDB'
 })
 }
 connection.connect(function (err) {
